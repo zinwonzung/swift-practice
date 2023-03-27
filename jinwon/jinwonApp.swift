005c617d1ec52bd004f2281ac8f1cc0218fcf57b
Jinwon Jung <113844225+zinwonzung@users.noreply.github.com>
2023-03-27 22:53:24 +0900

    Initial Commit

---
+++ jinwon/jinwonApp.swift
@@ -0,0 +1,20 @@
+//
+//  jinwonApp.swift
+//  jinwon
+//
+//  Created by 정진원 on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct jinwonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
